fa3fa2bf2f31019bceed12b046f7cc1ab43869dc
Gabrie3l3 <134063073+Gabrie3l3@users.noreply.github.com>
2024-04-27 20:26:37 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,5 +1,5 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
- let tamanhoSenha = 12;
+ let tamanhoSenha = 5;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
@@ -9,9 +9,10 @@ botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho() {
     if (tamanhoSenha > 1)
    // tamanhoSenha = tamanhoSenha + 1;
-  tamanhoSenha++
+  tamanhoSenha--;
 }
     numeroSenha.textContent = tamanhoSenha;
+gerasenha();
 }
 function aumentaTamanho() {
     if (tamanhoSenha < 30) {
@@ -19,7 +20,28 @@ function aumentaTamanho() {
      tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+gerasenha();
 }
 console.log(botoes);
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+
+function geraSenha(){
+let senha='';
+ for(let i=0;i<tamanhosenha;i++){
+ let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+numeroAleatorio=math.floor(numeroAleatorio);
+ senha=senha+letrasmaiusculas[numerosAleatorio];
+  
+}
+campoSenha.value = senha;
+
+
+
+
+
 
 
