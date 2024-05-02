e065b09716619e2338ff0798a5c136b2e16bd459
Gabrie3l3 <134063073+Gabrie3l3@users.noreply.github.com>
2024-05-02 12:49:27 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,12 +1,12 @@
-const numeroSenha = document.querySelector('parametro-senha__texto');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
-const botoes = document.querySelectorAll('parametro-senha__botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho()
+function diminuiTamanho(){
     if (tamanhoSenha > 1){
        // tamanhoSenha = tamanhoSenha-1;
         tamanhoSenha--;
