8f8ebf15105fff94d2e2c51680658155773bcc97
Gabrie3l3 <134063073+Gabrie3l3@users.noreply.github.com>
2024-05-02 12:53:59 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -55,7 +55,7 @@ function geraSenha(){
        console.log(alfabeto);
        let senha = '';
        for (let i = 0; i < tamanhoSenha;i++){
-    let numeroAleatorio = Math.random()alfabeto.length;
+    let numeroAleatorio = Math.random()*alfabeto.length;
     numeroAleatorio = Math.floor(numeroAleatorio);
     senha = senha + alfabeto[numeroAleatorio];
     }
