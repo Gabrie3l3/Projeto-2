e7c13d82ba65201a9d5e08bdee26b18f48ad9715
Gabrie3l3 <134063073+Gabrie3l3@users.noreply.github.com>
2024-05-02 12:55:29 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -14,6 +14,7 @@ function diminuiTamanho(){
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
+
      function aumentaTamanho(){
     if (tamanhoSenha < 20){
        // tamanhoSenha = tamanhoSenha+1;
